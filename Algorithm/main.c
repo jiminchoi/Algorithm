29ce68e6a77767c5cc38c55490c68904649e7da8
ChoiJimin <Producer@choijimin-no-macbook-pro.local>
2016-07-27 01:07:25 +0900

    Initial Commit

---
+++ Algorithm/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Algorithm
+//
+//  Created by ChoiJimin on 2016/07/27.
+//  Copyright © 2016年 ChoiJimin. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
